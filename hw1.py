b96a90d2a9b8444a5b0d80a1ff9568bb89c3216c
drh-14 <darrenh1134@gmail.com>
2025-02-19 17:16:48 -0500

    implemented pseudorandom number generator

--- hw1.py
+++ hw1.py
@@ -1,42 +1,43 @@
 
-import math
 import numpy as np
-import random
+import time
 
 def answer(value, iterations, fpOps):
     return f'Approximate Solution: {value}, Iterations: {iterations}, Approximate Number of Floating Point Operations: {fpOps}'
 
+def randomUniform(a,b):
+    x1,x2 = time.time(), time.time()
+    return 0.25 * ((x1 + x2) % 1) + 0.50
+
 # Question 1
 class Q1:
     def __init__(self):
-        self.f = lambda x: np.exp((-x ** 3)) - (x ** 4) - np.sin(x)
-        self.TOL = 0.5 * (10 ** -4)
+        self.f = lambda x: np.exp(np.power(-x, 3)) - np.power(x, 4) - np.sin(x)
+        self.TOL = 0.5 * np.power(10.0, -4)
         
     #Bisection Method   
     def method1(self):
         iterations = 0
-        operations = 0
         a,b = -1, 1
         while (b - a) / 2 > self.TOL:
             iterations += 1
             c = (a + b) / 2
-            operations += 1
             if self.f(c) == 0:
                 break
             elif self.f(a) * self.f(c) < 0:
-                operations += 1
                 b = c
             else:
                 a = c
-        operations += 1
         return (a + b) / 2
     
     #Newton's Method
     def method2(self):
-        deriv = lambda x: -3 * (x ** 2) * np.exp(-x ** 3) - (4 * (x ** 3)) - np.cos(x)
-        x = 0 
+        deriv = lambda x: -3 * np.power(x, 2) * np.exp(np.pow(-x, 3)) - (4 * np.pow(x, 3)) - np.cos(x)
+        x = 0
+        iterations = 0 
         while abs(self.f(x)) >= self.TOL:
             x -= (self.f(x) / deriv(x))
+            iterations += 1
         return x
     
     #Secant Method
@@ -54,7 +55,7 @@ class Q1:
     def method4(self):
         iterations = 0
         while(True):
-            x = random.uniform(0.50, 0.75)
+            x = randomUniform(0.50, 0.75)
             iterations += 1
             if abs(self.f(x)) < self.TOL:
                 return x
